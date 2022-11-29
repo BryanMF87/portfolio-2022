d3ada009890169212307999373bd2cc8fa730fb4
BryanMF87 <bmfink87@gmail.com>
2022-11-29 05:31:40 -0500

    directly targeted contact a links as black, added react-favicon and favicon.png to assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { useState } from 'react';
+import Favicon from "react-favicon";
 import ListOfProjects from './components/Projects/listOfProjects';
 // import components and pages
 import Navigation from './components/Navigation/Navigation';
 import HomePage from './pages/homePage/homePage';
 import Sidebar from './components/Sidebar/Sidebar';
 import Footer from './components/Footer/Footer';
+// import images
+import favIcon from '../src/assets/favicon.png';
 
 function App() {
 
@@ -14,6 +17,7 @@ function App() {
 
   return (
     <div className="App">
+      <Favicon url={favIcon}></Favicon>
       <Navigation />
       <HomePage 
         isOpen={isOpen}
